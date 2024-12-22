2d8de6d6a48afd4f16d2794b807b6132ee46653e
Daanish ilahi <daanishsumkesula@gmail.com>
2024-12-22 23:26:02 +0530

    Update application.js

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 import config from '../config';
-
+  
 export default DS.JSONAPIAdapter.extend({
   host: config.endpoints.server.host,
   namespace: config.endpoints.server.namespace
